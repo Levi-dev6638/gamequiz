f4519a3a1a0eb5306a12127d36fbc898ec83063d
devlevi <devgeral116@gmail.com>
2025-12-01 21:43:20 -0300

    feat: created main features related to questions and added dark mode theme.

--- quiz_functions.py
+++ quiz_functions.py
@@ -1,9 +1,17 @@
 import random
 import json
 import os 
+from tkinter import messagebox
+
+
+score = [0,0]
+players = ["jogador1","jogador2"] 
+current_player = 0
+current_rounds = 0 
+perguntas = []
 
 # it created the questions archive in case it didn't already exist
-QUESTION_FILE = "questions/quizquestions.json" 
+QUESTION_FILE = "questions/quiz_data.json" 
 os.makedirs("questions", exist_ok=True)
 
 
@@ -17,9 +25,65 @@ def load_data():
                 return []
     return []
 
+def finalizar_quiz():
+    if score[0] > score[1]:
+        vencedor = "player 1 wins"
+    elif score[1]> score[0]:
+        vencedor = "player 2 wins"
+    else:
+        vencedor = "empate"
+
+    messagebox.showinfo(
+        "fim do quiz",
+        f"placar final:\n"
+        f"jogador 1: {score[0]}\n"
+        f"player 2 : {score [1]}\n"
+        f"{vencedor}"
+    )
+
+
+def show_questions ():
+    question = perguntas[current_rounds]
+
+    lbl_question.config(text=question["pergunta"])
+    lbl_player.config(text = f"vez de {players[current_player]}")
+    lbl_pontos.config(text=f"J1: {score[0]} / J2: {score[1]} / Round: {current_rounds+1}")
+
+    resposta1.config(text=pergunta["opcoes"][0])
+    resposta2.config(text=pergunta["opçoes"][1])
+    resposta3.config(text=pergunta["opçoes"][3])
+
+
+def responder(resposta):
+    pergunta = perguntas[current_rounds]
+
+    verificar_resposta(resposta,pergunta)
+    next_move()
 #function to select randomly
-def select_random(lista):
-    return random.choice(lista)
+def select_random():
+    global current_player
+    current_player = random.randint(0,1)
+
+
+def verificar_resposta(resposta_do_usuario,pergunta):
+    global score,current_player
+
+    if resposta_do_usuario == pergunta["resposta"]:
+        score +=1
+        return
+
+def next_move():
+    global current_rounds
+
+    select_random()
+
+    if current_player == 0:
+        current_rounds +=1
+
+    if current_rounds < len(perguntas):
+        show_questions()
+    else:
+        finalizar_quiz()
 
 
 #function that randomly selects questions
--- quiz_interface.py
+++ quiz_interface.py
@@ -8,22 +8,43 @@ root.geometry("900x850")
 root.config(bg="#e8e8e8")
 
 def starting_the_game():
-    start = tk.Toplevel()
-    start.title("quiz interativo")
-    start.geometry("900x850")
-    start.config(bg="#e8e8e8")
+    fm_start = tk.Frame(root, bg="#ffffff", bd=2, relief="groove",width=5,height=50)
+    fm_start.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
 
-    lbl_pergunta = tk.Label(start,text="gnfdgnfgnjkgfgdfjkgnfkjgfkgbfkjgbfkjgbfkdjgbfdkjbgfdkj",width=40,font=("Arial",14))
+    lbl_pergunta = tk.Label(fm_start,text="",width=40,font=("Arial",14))
     lbl_pergunta.grid(row=0,column=2,padx=5,pady=30)
 
-    resposta1 = tk.Radiobutton(start,text="r")
+    resposta1 = tk.Radiobutton(fm_start,text="r")
     resposta1.grid(row=1,column=0,padx=5,pady=30)
 
-    resposta2 = tk.Radiobutton(start,text="r")
+    resposta2 = tk.Radiobutton(fm_start,text="r")
     resposta2.grid(row=2,column=0,padx=5,pady=30)
 
-    resposta3 = tk.Radiobutton(start,text="r")
+    resposta3 = tk.Radiobutton(fm_start,text="r")
     resposta3.grid(row=3,column=0,padx=5,pady=30)
+   
+
+dark_theme = False
+def toggle_theme():
+    global dark_theme
+    dark_theme = not dark_theme
+
+    if dark_theme:
+        root.configure(bg="#2E2E2E")
+        lbl_name_game.config(bg="#2E2E2E", fg="white")
+        lbl_rounds.config(bg="#2E2E2E", fg="white")
+        lbl_nameplayers.config(bg="#2E2E2E", fg="white")
+        bt_alttema.config(bg="#555555", fg="white")
+        bt_start.config(bg="#555555", fg="white")
+        
+    else:
+        root.configure(bg="white")
+        lbl_name_game.config(bg="#c1c1c4", fg="black")
+        lbl_nameplayers.config(bg="#c1c1c4", fg="black")
+        lbl_rounds.config(bg="#c1c1c4", fg="black")
+        bt_alttema.config(bg="#0e10a3", fg="black")
+        bt_start.config(bg="#0e10a3", fg="black")
+
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
@@ -45,19 +66,19 @@ entry_player1.grid(row=2,column=0,padx=5,pady=30,)
 entry_player2 = tk.Entry(frame,justify="center",font=("arial",14),width=30)
 entry_player2.grid(row=3,column=0,padx=5,pady=30)
 
-lbl_nameplayers = tk.Label(frame,text="number of rounds:",width=40,font=("Arial",14))
-lbl_nameplayers.grid(row=4,column=0,padx=5,pady=10)
+lbl_rounds = tk.Label(frame,text="number of rounds:",width=40,font=("Arial",14))
+lbl_rounds.grid(row=4,column=0,padx=5,pady=10)
 
 entry_player2 = tk.Entry(frame,justify="center",font=("arial",14))
 entry_player2.grid(row=5,column=0,padx=5,pady=30)
 
 bt_alttema = tk.Button(frame, text="Alterar Tema", bg="#0d097f", fg="#FFFFFF",
-                 font=("Arial", 14, "bold"),height=2,width=30)
+                 font=("Arial", 14, "bold"),height=2,width=30,command=toggle_theme)
 bt_alttema.grid(row=6, column=0, padx=5, pady=30)
 
-bt_alttema = tk.Button(frame, text="Iniciar Jogo", bg="#0b148b", fg="#ffffff",
+bt_start = tk.Button(frame, text="Iniciar Jogo", bg="#0b148b", fg="#ffffff",
                  font=("Arial", 14, "bold"),command=starting_the_game,height=2,width=30)
-bt_alttema.grid(row=7, column=0, padx=5, pady=30)
+bt_start.grid(row=7, column=0, padx=5, pady=30)
 
 
 
